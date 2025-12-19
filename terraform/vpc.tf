49ff032342866afaf1ffd003625aed904f23911e
natto447 <nattooliveira7@gmail.com>
2025-12-19 15:27:47 -0300

    chore: terraform fmt

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,35 +1,35 @@
 resource "aws_vpc" "this" {
-cidr_block = "10.0.0.0/16"
-tags = {
-Name = "${var.project_name}-vpc"
-}
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
 }
 
 
 resource "aws_subnet" "public" {
-vpc_id = aws_vpc.this.id
-cidr_block = "10.0.1.0/24"
-map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 }
 
 
 resource "aws_internet_gateway" "igw" {
-vpc_id = aws_vpc.this.id
+  vpc_id = aws_vpc.this.id
 }
 
 
 resource "aws_route_table" "public" {
-vpc_id = aws_vpc.this.id
+  vpc_id = aws_vpc.this.id
 
 
-route {
-cidr_block = "0.0.0.0/0"
-gateway_id = aws_internet_gateway.igw.id
-}
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
 
 
 resource "aws_route_table_association" "public" {
-subnet_id = aws_subnet.public.id
-route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
